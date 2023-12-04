b4bb1d3244c163fdc87b746267f0e59c589f7e9a
DBrush <coldnebraska@gmail.com>
2023-12-04 17:34:08 -0600

    added heroku port

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const path = require("path")
 const noteData = require("./db/db.json")
 const api = require("./routes/index.js")
 
-const PORT = 3001
+const PORT = process.env.PORT || 3001
 
 const app = express()
 
